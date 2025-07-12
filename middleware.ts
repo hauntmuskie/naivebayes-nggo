b7c7b877fc59889e4897dd2823e66762a2cfa77a
hauntmuskie <rioclasher4@gmail.com>
2025-07-12 09:03:16 +0700

    feat: add zod dependency and update public routes in middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { decrypt } from "@/lib/session";
 
 const protectedRoutes = ["/admin"];
-const publicRoutes = ["/login", "/"];
+const publicRoutes = ["/login"];
 
 export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
